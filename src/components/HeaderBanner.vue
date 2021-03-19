d5b048f92d763bc96fa4191ec862c797f2b361be
lukerhys <hello@lukemelvin.com>
2021-03-19 08:31:09 +0000

    simple menu banner

--- src/components/HeaderBanner.vue
+++ src/components/HeaderBanner.vue
@@ -2,61 +2,62 @@
 <div class="contianer">
     <div id="header" class="main">
 
-        <h1>Luke <br /> Melvin.</h1>
+        <h1>LukeMelvin.</h1>
         <img v-if="showMenuIcon" v-on:click="popOutNavHidden = !popOutNavHidden" class="menu-icon" :src="require(`@/assets/icons/menu_icon.png`)" />
     </div>
     <div class="info-banner">
         <h4>This webiste is still under contruction. Please drop us an <a href="mailto:hello@lukemelvin.com?subject=Your%website%is%broken.%I%need%more%info!">eMail</a> if you have any questions or can't find what you need.</h4>
     </div>
-    <div class="pop-out-nav">
+    <!-- <div class="pop-out-nav">
     <pop-out-nav v-if="!popOutNavHidden" />
-    </div>
+    </div> -->
 </div>
 </template>
 
 <script>
-import PopOutNav from './PopOutNav.vue'
-    export default {
-        name: 'header-banner',
-        data: function () {
-            return {
-                showMenuIcon: true,
-                popOutNavHidden: true
-            }
-        },
-        components: {
-            "pop-out-nav": PopOutNav
-        },
-        mounted(){
-            window.onload = function() {
-                setTimeout(() => {resizeBanner()}, 2000);
-                };
+// import PopOutNav from './PopOutNav.vue'
+//     export default {
+//         name: 'header-banner',
+//         data: function () {
+//             return {
+//                 showMenuIcon: true,
+//                 popOutNavHidden: true
+//             }
+//         },
+//         components: {
+//             "pop-out-nav": PopOutNav
+//         },
+//         mounted(){
+//             window.onload = function() {
+//                 setTimeout(() => {resizeBanner()}, 2000);
+//                 };
                 
-                function resizeBanner() {
-                if (onload) {
-                    document.getElementById("header").style.fontSize = "100%";
-                    document.getElementById("header").style.height= "10vh";
-                } 
+//                 function resizeBanner() {
+//                 if (onload) {
+//                     document.getElementById("header").style.fontSize = "100%";
+//                     document.getElementById("header").style.height= "10vh";
+//                 } 
 
-            }
-        },
-        methods: {
-        }
-    }
+//             }
+//         },
+//         methods: {
+//         }
+//     }
 </script>
 
 <style lang="css" scoped>
 #header {
-background-color: #FABC2A;
+    background-color: #FFFFFF;
+    /* border-bottom: #FABC2A solid 2px; */
     font-size: 10vh;
-    height: 100vh;
+    height: 10vh;
     width: 100%;
     position: fixed;
     top: 0;
     transition: 2s;
     max-width: 100vw;
     z-index: 1000;
-    box-shadow: 0em 0.1em 1em rgba(0, 0, 0, .4);
+    box-shadow: 0em 0.1em 1em rgba(0, 0, 0, 0.1);
     display: flex;
     align-items: center;
     justify-content: space-between;
@@ -66,6 +67,7 @@ h1 {
     margin: 0;
     font-weight: bold;
     line-height: 100%;
+    font-size: 50%; 
     color: #404040;
     padding-left: 2%;
 }
