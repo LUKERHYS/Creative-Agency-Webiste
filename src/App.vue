17f2ff16ec91a976eb7234433dcaeecbdeda52fb
lukerhys <hello@lukemelvin.com>
2021-03-19 08:33:04 +0000

    installing vue router

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,32 @@
 <template>
   <div id="app">
-
-    <header-banner />
-   
-    <topic-list :topics="topics"/>
-    <!-- <info-card v-if="!isHidden" transition="expand" :details="this.infoCard" id="info" /> -->
-
-    <!-- <full-footer /> -->
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import HeaderBanner from './components/HeaderBanner.vue'
-import TopicList from './components/TopicList.vue'
-// import InfoCard from './components/InfoCard.vue'
-import FullFooter from './components/Footer.vue'
-
-
-import {eventBus} from './main.js'
-
-export default {
-  name: 'App',
-  components: {
-    'header-banner': HeaderBanner,
-    'topic-list': TopicList,
-    // 'info-card' : InfoCard,
-    'full-footer' : FullFooter
-  },
-  data: function() {
-    return {
-      topics: null,
-      infoCard: null,
-      isHidden: true
-    }
-  },
-  mounted() {
-    fetch('https://cms.lukemelvin.com/topics')
-    .then(res => res.json())
-    .then(payload => {this.topics = payload})
-
-
-
-      eventBus.$on('topic-selected', (infoCardDetails) => {
-        let foundIndex = this.topics.findIndex(currentId => currentId.id == infoCardDetails.id);
-        this.topics[foundIndex]['infoCard'] = infoCardDetails;
-        // this.infoCard= infoCardDetails;
-      })
-      eventBus.$on ('hidden-state', (stateChange) => {
-        this.isHidden = stateChange;
-      })
-      eventBus.$on ('hidden-state', (stateChange) => {
-        this.isHidden = stateChange;
-      })
-  }
-}
-</script>
-
 <style>
 #app {
-  font-family: Arial, sans-serif;
-  /* font: 2.5vh  Helvetica, sans-serif; */
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
